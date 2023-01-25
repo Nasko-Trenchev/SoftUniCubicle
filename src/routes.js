d41d834cd938134c083d8824bcbd0dfe9da91f58
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-01-25 18:14:27 +0200

    Add search option

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,11 @@ const router = require("express").Router()
 
 router.get("/", homeControler.getHomePage);
 router.get("/about", homeControler.getAboutPage);
+router.get('/404', homeControler.getErrorPage);
 
 router.get('/create', cubeControler.getCreateCube);
 router.post('/create', cubeControler.postCreateCube)
 router.get('/details/:cubeId', cubeControler.getDetails)
+
 
 module.exports = router;
