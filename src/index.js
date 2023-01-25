62141040648f77b32fe0095d846f64a73459dab4
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-01-25 12:02:19 +0200

    Setup express

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const config = require("./config");
+
+const app = express();
+
+app.get("/", (req,res) =>{
+
+    res.send("Homepage")
+});
+
+app.listen(config.PORT, () => {
+    console.log(`Server is listening on port ${config.PORT}...`);
+})
