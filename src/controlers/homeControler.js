ba7b74f5630c6646a0d5c4a9f6defb8fe86d41c7
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-01-30 13:23:03 +0200

    Show cubes in homePage from Db

--- src/controlers/homeControler.js
+++ src/controlers/homeControler.js
@@ -1,8 +1,11 @@
-const db = require("../db.json");
+//const db = require("../db.json");
+const Cube = require('../models/Cube');
 
-exports.getHomePage = (req, res) =>{
+exports.getHomePage = async (req, res) =>{
     const {search, from, to} = req.query;   
-    let cubes = db.cubes;
+
+    let cubes = await Cube.find().lean();
+
     if(search){
         cubes = cubes.filter(cube => cube.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
     }
