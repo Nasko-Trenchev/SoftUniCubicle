dcc2a00b34979c71ee48c961bc7d9ef83e92fa71
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-01-30 15:58:25 +0200

    Paths fixed

--- src/routes.js
+++ src/routes.js
@@ -8,14 +8,13 @@ router.get("/", homeControler.getHomePage);
 router.get("/about", homeControler.getAboutPage);
 router.get('/404', homeControler.getErrorPage);
 
-router.get('/create', cubeControler.getCreateCube);
-router.post('/create', cubeControler.postCreateCube)
+router.get('/cubes/create', cubeControler.getCreateCube);
+router.post('/cubes/create', cubeControler.postCreateCube)
 router.get('/cubes/:cubeId/details', cubeControler.getDetails)
-
 router.get('/cubes/:cubeId/attach', cubeControler.getAttachAccessory);
 router.post('/cubes/:cubeId/attach', cubeControler.postAttachAccessory);
 
 
-router.use('/accessory', accesorryControler);
+router.use('/accessories', accesorryControler);
 
 module.exports = router;
