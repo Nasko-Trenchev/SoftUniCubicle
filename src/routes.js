f59a4257c8479bca5d832fa4eafe64a3c6f9e332
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-02-02 11:03:09 +0200

    Add login and register pages

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const cubeControler = require("./controlers/cubeControler")
 const homeControler = require("./controlers/homeControler")
 const accesorryControler = require('./controlers/accessoryControler');
+const authControler = require('./controlers/authController');
 
 const router = require("express").Router()
 
@@ -8,12 +9,13 @@ router.get("/", homeControler.getHomePage);
 router.get("/about", homeControler.getAboutPage);
 router.get('/404', homeControler.getErrorPage);
 
+router.use('/', authControler);
+
 router.get('/cubes/create', cubeControler.getCreateCube);
 router.post('/cubes/create', cubeControler.postCreateCube)
 router.get('/cubes/:cubeId/details', cubeControler.getDetails)
 router.get('/cubes/:cubeId/attach', cubeControler.getAttachAccessory);
 router.post('/cubes/:cubeId/attach', cubeControler.postAttachAccessory);
-
 
 router.use('/accessories', accesorryControler);
 
